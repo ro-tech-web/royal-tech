3d8d386360989915c627d8ebcffd50a2bb7fdcc6
ro-tech-web <samuelodemba84@gmail.com>
2025-10-25 19:06:15 +0300

    Add files via upload

---
+++ log.php
@@ -0,0 +1,13 @@
+<?php
+if($_SERVER["REQUESTED-METHOD"]=="POST"){
+    name=$post["name"];
+    email=$post["name"];
+    password=$post["password"]
+    if ($name=="admin"$$ $email=="admin"$$ $password=="admin")
+{
+echo"welcome to our services"<br>}
+else {
+    echo"invalid credential<br>
+    echo"<a href="index.html">try again</a>
+}
+}
